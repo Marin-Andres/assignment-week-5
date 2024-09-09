9b53465bc845e50fa80cbaa1b16404fae98e597b
Andres Marin <andres.marin@gmx.com>
2024-09-09 16:32:23 +1000

    Extend the sales controller to use dtos that show the store name, customer name and product name and not a complex object

--- Controllers/SaleController.cs
+++ Controllers/SaleController.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using IndustryConnect_Week5_WebApi.Models;
+using IndustryConnect_Week5_WebApi.Mappers;
+using IndustryConnect_Week5_WebApi.Dtos;
 
 namespace IndustryConnect_Week5_WebApi.Controllers
 {
@@ -22,37 +24,54 @@ namespace IndustryConnect_Week5_WebApi.Controllers
 
         // GET: api/Sale
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Sale>>> GetSales()
+        public async Task<ActionResult<IEnumerable<SaleDto>>> GetSales()
         {
-            return await _context.Sales.Include(p => p.Product)
-                .Include(c => c.Customer).ToListAsync();
+            var _sales = await _context.Sales.
+                Include(p => p.Product).
+                Include(c => c.Customer). 
+                Include(st => st.Store).
+                Select(s => SaleMapper.EntityToDto(s)).ToListAsync();
+            
+            if(_sales.Count > 0)
+            {
+                return Ok(_sales);
+            }
+            else
+            {
+                return BadRequest("There are no sales at the moment");
+            }         
         }
 
         // GET: api/Sale/5
         [HttpGet("{id}")]
-        public async Task<ActionResult<Sale>> GetSale(int id)
+        public async Task<ActionResult<SaleDto>> GetSale(int id)
         {
-            var sale = await _context.Sales.FindAsync(id);
+            var sale = await _context.Sales.Where(s => s.Id == id).
+                Include(p => p.Product).
+                Include(c => c.Customer).
+                Include(st => st.Store).
+                FirstOrDefaultAsync();
 
             if (sale == null)
             {
                 return NotFound();
             }
 
-            return sale;
+            return SaleMapper.EntityToDto(sale);
         }
 
         // PUT: api/Sale/5
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPut("{id}")]
-        public async Task<IActionResult> PutSale(int id, Sale sale)
+        public async Task<IActionResult> PutSale(int id, SaleDto sale)
         {
             if (id != sale.Id)
             {
                 return BadRequest();
             }
 
-            _context.Entry(sale).State = EntityState.Modified;
+            var entity = SaleMapper.DtoToEntity(sale);
+            _context.Entry(entity).State = EntityState.Modified;
 
             try
             {
@@ -70,15 +89,17 @@ namespace IndustryConnect_Week5_WebApi.Controllers
                 }
             }
 
-            return NoContent();
+            return Ok(SaleMapper.EntityToDto(entity));
         }
 
         // POST: api/Sale
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPost]
-        public async Task<ActionResult<Sale>> PostSale(Sale sale)
+        public async Task<ActionResult<Sale>> PostSale(SaleDto sale)
         {
-            _context.Sales.Add(sale);
+            var entity = SaleMapper.DtoToEntity(sale);
+
+            _context.Sales.Add(entity);
             await _context.SaveChangesAsync();
 
             return CreatedAtAction("GetSale", new { id = sale.Id }, sale);
